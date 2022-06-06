4df25f24b58634ab6ad278b47bb6d6894bbbdb25
Allan <allan.esib@gmail.com>
2022-06-06 19:19:33 +0100

    Created about content

--- client/src/PortfolioContainer/About/About.js
+++ client/src/PortfolioContainer/About/About.js
@@ -3,10 +3,27 @@ import './About.css';
 
 export default function About() {
     return (
-        <div className='componente'>
-            <section>
-                <h1>hi</h1>
-            </section>
-        </div>
+        <section id="about-container" className='componente'>
+            <h2 className='magic_underline'>Me, Mysefl & I</h2>
+            <h3>
+                Hello, my name is <b>Allan Bastian Espinoza Ibáñez</b>, a 90's child born in Valparaiso, Viña del Mar in Chile 
+                and raised in Gran Canaria, Spain. 
+            </h3>
+            <p>
+                Since I can remember, I have been a curious person about everything related to the world of tech.
+                This has made me a multidisciplinary and organized person.
+                Before I started studying, I learned a lot about web development in a self-taught way. 
+                These first steps made me get into the world of web development.
+            </p>
+            <p>
+                A few years later, thanks to this knowledge, I started studying web develpment in 2019, 
+                at the professional training center, Grupo MBC in Las Palmas de Gran Canaria. During my training, I stood out for my grades, my teamwork and my ability to solve problems.
+                When I finished the course, the same center where I studied hired me to create their learning platforms with Moodle,
+                where I have been working until now. 
+            </p>
+            <p>
+                Currently, I am open to listen to job offers that allow me to contribute and continue growing.
+            </p>
+        </section>
     )
 }
--- client/src/PortfolioContainer/Nav/Nav.js
+++ client/src/PortfolioContainer/Nav/Nav.js
@@ -14,61 +14,61 @@ export default function NavContainer() {
 
     const navControl = () => {
         const nav = document.getElementById("nav-right");
-        if(navState === false){
-            setNavState(true);
-            nav.classList.add("open-nav");
-            nav.classList.remove("close-nav");
-        }else{
+        const button_nav = document.getElementById("nav-drawer");
+        console.log(button_nav.animation);
+        if (navState === false) {
+            setNavState(true); //change nav state to true = open
+            nav.classList.remove("close-nav"); //remove the animation that closes the nav
+            nav.classList.add("open-nav"); //and add the animation that opens the nav
+        } else {
             setNavState(false);
-            nav.classList.add("close-nav");
             nav.classList.remove("open-nav");
+            nav.classList.add("close-nav");
         }
     }
 
     return (
-         <nav id='nav-container'>
-                <div id='nav-section'>
-                    <button onClick={navControl} id='nav-drawer' className={!navState ? 'part-nav' : 'part-nav actived-button-nav'}>
-                        <FontAwesomeIcon icon={!navState ? faBars : faXmark} size='2x' />
-                    </button>
-                    <div id='nav-left' className='part-nav'>
-                        <a href='.' id='enlace-home'>
-                            <img id='imagen-nav' src={extraterrestre} alt='alien-cabecera'></img>
-                            <div>
-                                <p>Allan Espinoza</p>
-                                <p>Web Developer</p>
-                            </div>
+        <nav id='nav-container'>
+            <button onClick={navControl} id='nav-drawer' className='part-nav'>
+                <FontAwesomeIcon icon={!navState ? faBars : faXmark} size='2x' />
+            </button>
+            <div id='nav-left' className='part-nav'>
+                <a href='.' id='enlace-home'>
+                    <img id='imagen-nav' src={extraterrestre} alt='alien-cabecera'></img>
+                    <div>
+                        <p>Allan Espinoza</p>
+                        <p>Web Developer</p>
+                    </div>
+                </a>
+            </div>
+            <div id='nav-right' className='part-nav'>
+                <ul>
+                    <li onClick={navControl}>
+                        <a href='#home-container'>
+                            <span className='icon-container'><FontAwesomeIcon icon={faHouseUser} size='lg' /></span>
+                            <span className='magic_underline'>Home</span>
                         </a>
-                    </div>
-                    <div id='nav-right' className='part-nav'>
-                        <ul>
-                            <li>
-                                <a href='#home-container'>
-                                    <span className='icon-container'><FontAwesomeIcon icon={faHouseUser} size='lg'/></span>
-                                    <span className='magic_underline'>Home</span>
-                                </a>
-                            </li>
-                            <li>
-                                <a href='#about-me-section'>
-                                    <span className='icon-container'><FontAwesomeIcon icon={faCrown} size='lg'/></span>
-                                    <span className='magic_underline'>About me</span>
-                                </a>
-                            </li>
-                            <li>
-                                <a href='#work'>
-                                    <span className='icon-container'><FontAwesomeIcon icon={faHistory} size='lg'/></span>
-                                    <span className='magic_underline'>Work</span>
-                                </a>
-                            </li>
-                            <li>
-                                <a href='#home-section'>
-                                    <span className='icon-container'><FontAwesomeIcon icon={faMessage} size='lg'/></span>
-                                    <span className='magic_underline'>Contact</span>
-                                </a>
-                            </li>
-                        </ul>
-                    </div>
-                </div>
-            </nav>
+                    </li>
+                    <li onClick={navControl}>
+                        <a href='#about-container'>
+                            <span className='icon-container'><FontAwesomeIcon icon={faCrown} size='lg' /></span>
+                            <span className='magic_underline'>About me</span>
+                        </a>
+                    </li>
+                    <li onClick={navControl}>
+                        <a href='#work'>
+                            <span className='icon-container'><FontAwesomeIcon icon={faHistory} size='lg' /></span>
+                            <span className='magic_underline'>Work</span>
+                        </a>
+                    </li>
+                    <li onClick={navControl}>
+                        <a href='#home-section'>
+                            <span className='icon-container'><FontAwesomeIcon icon={faMessage} size='lg' /></span>
+                            <span className='magic_underline'>Contact</span>
+                        </a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
     )
 };
