6847c9f7caacc785ea981949849502372bc4a587
Allan <allan.esib@gmail.com>
2022-06-21 01:01:18 +0100

    Modified all h3 of sections

--- client/src/PortfolioContainer/Work/Projects/Projects.js
+++ client/src/PortfolioContainer/Work/Projects/Projects.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './Projects.css';
-import glamorous, { withTheme } from 'glamorous';
+import glamorous from 'glamorous';
 
 export default function Projects(props){
     const project = props.project;
